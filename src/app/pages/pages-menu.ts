90c6c70c869c318eb1ecd1cf4ead534db274be42
netdevenv <36025410+netdevenv@users.noreply.github.com>
2018-02-05 18:32:50 +0530

    Initial Commit Last One
    Hello,
    
    Its initial commit here.

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,132 +2,128 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
+    title: 'Analysis',
     icon: 'nb-home',
     link: '/pages/dashboard',
     home: true,
   },
-  {
-    title: 'Filler Library',
-    icon: 'nb-edit',
-    link: '/pages/filler-library',
+   {
+    title: 'Game Overview',
+    icon: 'nb-title',
+    link: '/pages/dashboard',
   },
   {
-    title: 'FEATURES',
-    group: true,
-  },
-  {
-    title: 'UI Features',
+    title: 'Customer Analysis',
     icon: 'nb-keypad',
     link: '/pages/ui-features',
     children: [
       {
-        title: 'Buttons',
+        title: 'User Engagement',
         link: '/pages/ui-features/buttons',
       },
       {
-        title: 'Grid',
+        title: 'Churn Analysis',
         link: '/pages/ui-features/grid',
       },
       {
-        title: 'Icons',
+        title: 'Pricing Analysis',
         link: '/pages/ui-features/icons',
       },
-      {
-        title: 'Modals',
-        link: '/pages/ui-features/modals',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-      {
-        title: 'Tabs',
-        link: '/pages/ui-features/tabs',
-      },
+      // {
+      //   title: 'Modals',
+      //   link: '/pages/ui-features/modals',
+      // },
+      // {
+      //   title: 'Typography',
+      //   link: '/pages/ui-features/typography',
+      // },
+      // {
+      //   title: 'Animated Searches',
+      //   link: '/pages/ui-features/search-fields',
+      // },
+      // {
+      //   title: 'Tabs',
+      //   link: '/pages/ui-features/tabs',
+      // },
     ],
   },
   {
-    title: 'Forms',
+    title: 'Pricing Console',
     icon: 'nb-compose',
-    children: [
-      {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
-      },
-      {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
-      },
-    ],
+    // children: [
+    //   {
+    //     title: 'Form Inputs',
+    //     link: '/pages/forms/inputs',
+    //   },
+    //   {
+    //     title: 'Form Layouts',
+    //     link: '/pages/forms/layouts',
+    //   },
+    // ],
   },
   {
-    title: 'Components',
+    title: 'Revenue Estimation',
     icon: 'nb-gear',
-    children: [
-      {
-        title: 'Tree',
-        link: '/pages/components/tree',
-      }, {
-        title: 'Notifications',
-        link: '/pages/components/notifications',
-      },
-    ],
+    // children: [
+    //   {
+    //     title: 'Tree',
+    //     link: '/pages/components/tree',
+    //   }, {
+    //     title: 'Notifications',
+    //     link: '/pages/components/notifications',
+    //   },
+    // ],
   },
-  {
-    title: 'Maps',
-    icon: 'nb-location',
-    children: [
-      {
-        title: 'Google Maps',
-        link: '/pages/maps/gmaps',
-      },
-      {
-        title: 'Leaflet Maps',
-        link: '/pages/maps/leaflet',
-      },
-      {
-        title: 'Bubble Maps',
-        link: '/pages/maps/bubble',
-      },
-    ],
-  },
-  {
-    title: 'Charts',
-    icon: 'nb-bar-chart',
-    children: [
-      {
-        title: 'Echarts',
-        link: '/pages/charts/echarts',
-      },
-      {
-        title: 'Charts.js',
-        link: '/pages/charts/chartjs',
-      },
-      {
-        title: 'D3',
-        link: '/pages/charts/d3',
-      },
-    ],
-  },
-  {
-    title: 'Editors',
-    icon: 'nb-title',
-    children: [
-      {
-        title: 'TinyMCE',
-        link: '/pages/editors/tinymce',
-      },
-      {
-        title: 'CKEditor',
-        link: '/pages/editors/ckeditor',
-      },
-    ],
-  },
+  // {
+  //   title: 'Maps',
+  //   icon: 'nb-location',
+  //   children: [
+  //     {
+  //       title: 'Google Maps',
+  //       link: '/pages/maps/gmaps',
+  //     },
+  //     {
+  //       title: 'Leaflet Maps',
+  //       link: '/pages/maps/leaflet',
+  //     },
+  //     {
+  //       title: 'Bubble Maps',
+  //       link: '/pages/maps/bubble',
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'Charts',
+  //   icon: 'nb-bar-chart',
+  //   children: [
+  //     {
+  //       title: 'Echarts',
+  //       link: '/pages/charts/echarts',
+  //     },
+  //     {
+  //       title: 'Charts.js',
+  //       link: '/pages/charts/chartjs',
+  //     },
+  //     {
+  //       title: 'D3',
+  //       link: '/pages/charts/d3',
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'Editors',
+  //   icon: 'nb-title',
+  //   children: [
+  //     {
+  //       title: 'TinyMCE',
+  //       link: '/pages/editors/tinymce',
+  //     },
+  //     {
+  //       title: 'CKEditor',
+  //       link: '/pages/editors/ckeditor',
+  //     },
+  //   ],
+  // },
   {
     title: 'Tables',
     icon: 'nb-tables',
@@ -138,26 +134,26 @@ export const MENU_ITEMS: NbMenuItem[] = [
       },
     ],
   },
-  {
-    title: 'Auth',
-    icon: 'nb-locked',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
-  },
+  // {
+  //   title: 'Auth',
+  //   icon: 'nb-locked',
+  //   children: [
+  //     {
+  //       title: 'Login',
+  //       link: '/auth/login',
+  //     },
+  //     {
+  //       title: 'Register',
+  //       link: '/auth/register',
+  //     },
+  //     {
+  //       title: 'Request Password',
+  //       link: '/auth/request-password',
+  //     },
+  //     {
+  //       title: 'Reset Password',
+  //       link: '/auth/reset-password',
+  //     },
+  //   ],
+  // },
 ];
